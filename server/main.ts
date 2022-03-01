c28548f4e1baa6344f492773bc4f3af22b5186c5
Joxos <xujunhao61@163.com>
2022-03-01 16:07:46 +0800

    update

--- server/main.ts
+++ server/main.ts
@@ -1,8 +1,11 @@
+import { verify } from "crypto";
 import { Server } from "socket.io";
 let fs = require("fs");
 
 const io = new Server();
-let users = "";
+
+// read users file
+let users: any;
 fs.readFile('./users.json', 'utf8', (err: string, data: string) => {
     if (err) {
         console.info(`error reading users file: ${err}`);
@@ -12,6 +15,7 @@ fs.readFile('./users.json', 'utf8', (err: string, data: string) => {
     console.info(`read users storage file:\n${JSON.stringify(users, null, 2)}\n`);
 });
 
+// when connected
 io.on("connection", (socket) => {
     console.info(`${socket.id} connected`);
 
@@ -20,13 +24,24 @@ io.on("connection", (socket) => {
     });
 
     function verifyLogin(uid: string, password: string) {
-
+        for (let user of users) {
+            if (user.get("uid") == uid && user.get("password") == password) {
+                return true;
+            }
+        }
+        return false;
     };
     socket.on("login", (uid, password) => {
         console.info(`${socket.id}: login\n  uid: ${uid}\n  password: ${password}`)
+        if (verifyLogin(uid, password)) {
+            socket.emit("success");
+        } else {
+            socket.emit("failed");
+        }
     });
 
 });
 
+// listen on port 3000
 io.listen(3000);
 console.info("started listening at *:3000\n");
